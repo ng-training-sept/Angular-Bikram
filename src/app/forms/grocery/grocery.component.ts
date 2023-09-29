b642937136eade793bcaf912c121f6948c988bfd
Bikram Shrestha <bikram.shrestha@cotiviti.com>
2023-09-29 14:14:26 +0545

    signal

--- src/app/forms/grocery/grocery.component.ts
+++ src/app/forms/grocery/grocery.component.ts
@@ -1,35 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Card } from 'src/app/components/card/card.model';
 import { CardComponent } from 'src/app/components/card/card.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { environment } from 'src/environment';
+
 
 @Component({
   selector: 'app-grocery',
   standalone: true,
-  imports: [CommonModule,CardComponent],
+  imports: [CommonModule,CardComponent,HttpClientModule],
   templateUrl: './grocery.component.html',
   styleUrls: ['./grocery.component.scss']
 })
 export class groceryComponent implements OnInit {
+  grocery: Card[] = [];
+    private readonly http = inject(HttpClient);
+  
+    ngOnInit(): void {
+      this.http
+        .get<Card[]>(`${environment.baseUrl}/grocery`)
+        .subscribe(grocery => this.grocery = grocery);
+    }
 
-  groceryCards: Card[] = [];
+  // groceryCards: Card[] = [];
 
-  ngOnInit(): void {
-      this.groceryCards = [
-          {
-              id: '3',
-              name: 'Orange',
-              price: 150,
-              imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/f/f9/Orange_and_cross_section.jpg',
-              description: 'Orange Fruit'
-          },
-          {
-              id: '4',
-              name: 'Apple',
-              price: 50,
-              imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/15/Red_Apple.jpg',
-              description: 'Apple Fruit'
-          }
-      ];
-  }
+  // ngOnInit(): void {
+  //     this.groceryCards = [
+  //         {
+  //             id: '3',
+  //             name: 'Orange',
+  //             price: 150,
+  //             imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/f/f9/Orange_and_cross_section.jpg',
+  //             description: 'Orange Fruit'
+  //         },
+  //         {
+  //             id: '4',
+  //             name: 'Apple',
+  //             price: 50,
+  //             imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/15/Red_Apple.jpg',
+  //             description: 'Apple Fruit'
+  //         }
+  //     ];
+  // }
 }
--- src/app/forms/sports/sports.component.ts
+++ src/app/forms/sports/sports.component.ts
@@ -1,35 +1,59 @@
 import { CommonModule } from "@angular/common";
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, inject } from "@angular/core";
 import { Card } from 'src/app/components/card/card.model';
 import { CardComponent } from "src/app/components/card/card.component";
+import { SportsStore } from "./sports.store";
+import { HttpClientModule } from "@angular/common/http";
+import { AuthService } from "src/app/auth/auth.service";
 
 @Component({
   selector: 'app-sports',
   standalone: true,
+  providers: [
+    // AuthService,
+    SportsStore
+  ],
   templateUrl: './sports.component.html',
   styleUrls: ['./sports.component.scss'],
-  imports: [CommonModule, CardComponent]
+  imports: [CommonModule, CardComponent,HttpClientModule]
 })
 export class SportsComponent implements OnInit {
-
-  sportCards: Card[] = [];
+  sports: Card[] = [];
+  private readonly authService = inject(AuthService);
+  readonly sportsStore = inject(SportsStore);
 
   ngOnInit(): void {
-      this.sportCards = [
-          {
-              id: '1',
-              name: 'Football',
-              price: 1500,
-              imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/1d/Football_Pallo_valmiina-cropped.jpg',
-              description: 'Football description'
-          },
-          {
-              id: '2',
-              name: 'Football Boot',
-              price: 6000,
-              imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/AdidasEtruscoBoot.jpg/230px-AdidasEtruscoBoot.jpg',
-              description: 'Football description'
-          }
-      ];
+    this.sportsStore.fetchSports();
+    setTimeout(() => {
+      this.sportsStore.saveSport({
+        id: 'test 3',
+        name: 'Test 3',
+        description: 'test 3',
+        price: 603.55,
+        imageUrl: ''
+      });
+    }, 5000)
   }
+
+  // sportCards: Card[] = [];
+
+  // ngOnInit(): void {
+  //     this.sportCards = [
+  //         {
+  //             id: '1',
+  //             name: 'Football',
+  //             price: 1500,
+  //             imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/1d/Football_Pallo_valmiina-cropped.jpg',
+  //             description: 'Football description'
+  //         },
+  //         {
+  //             id: '2',
+  //             name: 'Football Boot',
+  //             price: 6000,
+  //             imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/AdidasEtruscoBoot.jpg/230px-AdidasEtruscoBoot.jpg',
+  //             description: 'Football description'
+  //         }
+  //     ];
+  // }
+
 }
