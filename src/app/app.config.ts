7b1e790135b9e852e2507d2b646d4d9b67c8ecbe
Bikram Shrestha <bikram.shrestha@cotiviti.com>
2023-09-29 10:07:30 +0545

    DI Form completed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,20 +5,25 @@ import { SportsComponent } from './forms/sports/sports.component';
 import { CardItemComponent } from './components/card/card-item/card-item.component';
 import { groceryComponent } from './forms/grocery/grocery.component';
 import { PageNotFoundComponent } from './forms/page-not-found/page-not-found.component';
+import { authGuard } from './auth/auth.constants';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   {path: 'sports', component: SportsComponent}, // Eagerly loading route
   { path: 'sports', loadComponent: () => import('./forms/sports/sports.component')
   .then(m => m.SportsComponent) }, // Lazily loading route
-  {path: 'grocery', component: groceryComponent}, 
   { path: 'sports/card-item/:id', component: CardItemComponent},
 
-  { path: '', redirectTo: '/', pathMatch: 'full' },
-  {path: 'grocery', component: groceryComponent}, 
-  { path: 'grocery', loadComponent: () => import('./forms/grocery/grocery.component')
-  .then(m => m.groceryComponent) },// Lazily loading route
-  { path: 'grocery/card-item/:id', component: CardItemComponent},
+
+  // {path: 'grocery', component: groceryComponent}, 
+  // { path: 'grocery', loadChildren: () => import('./forms/grocery/grocery.component')
+  // .then(m => m.groceryComponent) },// Lazily loading route
+  // { path: 'grocery/card-item/:id', component: CardItemComponent},
+
+  {path: 'grocery', 
+  canActivate: [authGuard],
+  loadComponent: () => import('./forms/grocery/grocery.component').then(m => m.groceryComponent)},
+
 
   { path: 'home', loadComponent: () => import('./forms/home/home.component')
   .then(m => m.HomeComponent) }, // Lazily loading route
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { ItemSaveUpdateComponent } from '../item-save-update/item-save-update.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatMenuModule, MatButtonModule, RouterModule],
+  imports: [CommonModule, MatToolbarModule, MatMenuModule, MatButtonModule, RouterModule,
+    MatDialogModule,MatIconModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-     
+  private readonly authService = inject(AuthService);
+  private readonly dialog = inject(MatDialog);
+  addNew(): void {
+    const dialogRef = this.dialog.open(ItemSaveUpdateComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      if (result?.data) {
+        console.log(result.data);
+        // emit update event and call service from parent to update card
+      }
+    });
+  }
+
+    login(): void {
+      this.authService.authenticate();
+    }
+  
 }
